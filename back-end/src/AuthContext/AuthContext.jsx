81f002720356427028b8b942f8caac5729eb636f
ram718 <ramsahith65@gmail.com>
2023-01-18 22:07:55 +0530

    day-2

---
+++ back-end/src/AuthContext/AuthContext.jsx
@@ -0,0 +1,7 @@
+import { createContext,useState } from "react";
+
+export const AuthContext = createContext();
+
+export default function AuthContextProvider({children}){
+
+}
---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AllRoutes from './components/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
