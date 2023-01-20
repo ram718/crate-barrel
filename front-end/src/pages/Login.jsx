9de1129dede3e7886e00f8ac226266eec664d3b2
ram718 <ramsahith65@gmail.com>
2023-01-20 22:27:41 +0530

    day-4

---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,98 @@
+import {
+    Button,
+    Checkbox,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
+    Image,
+    useToast
+  } from '@chakra-ui/react';
+  import {useState,useEffect,useContext} from "react";
+  import {AuthContext} from "../Context/AuthContext"
+  import {Link} from "react-router-dom";
+
+  function Toast() {
+    const toast = useToast()
+    return (
+    //   <Button
+    //     onClick={() =>
+          toast({
+            title: 'Logged-in Success',
+            description: "We've created your account for you.",
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+    //     }
+    //   >
+    //     Show Toast
+    //   </Button>
+    )
+  }
+  
+  export default function Login() {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const {isAuth,loginUser} = useContext(AuthContext);
+
+    const handleLogin = () => {
+        fetch(`https://reqres.in/api/login`,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({email,password})
+        })
+        .then((res) => res.json)
+        .then((res) => loginUser(Toast))
+        .catch((err) => console.log(err))
+    }
+    if(isAuth){
+
+
+        <Link to="/"></Link>
+    }
+    
+
+    return (
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+          <Stack spacing={4} w={'full'} maxW={'md'}>
+            <Heading fontSize={'2xl'}>Sign in to your account</Heading>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </FormControl>
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Link color={'blue.500'}>Forgot password?</Link>
+              </Stack>
+              <Button colorScheme={'blue'} variant={'solid'} onClick={handleLogin}>
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex flex={1}>
+          <Image
+            alt={'Login Image'}
+            objectFit={'cover'}
+            src={
+              'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+            }
+          />
+        </Flex>
+      </Stack>
+    );
+  }
--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,10 +1,87 @@
 import { useState,useEffect } from "react";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import SingleProduct from "./SingleProduct";
+import {Box,
+    Center,
+    useColorModeValue,
+    Heading,
+    Text,
+    Stack,
+    Image} from "@chakra-ui/react";
+
+//     const IMAGE =
+//   'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
+
+function ProductCard({id,title,category,price,image}) {
+  return (
+    <Center py={12}>
+      <Box
+        key={id}
+        role={'group'}
+        p={6}
+        maxW={'35%'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.800')}
+        boxShadow={'2xl'}
+        rounded={'lg'}
+        pos={'relative'}
+        zIndex={10}>
+        <Box
+          rounded={'lg'}
+          mt={-12}
+          pos={'relative'}
+          height='230px'
+          width="100%"
+          _after={{
+            transition: 'all .3s ease',
+            content: '""',
+            w: 'full',
+            h: 'full',
+            pos: 'absolute',
+            top: 5,
+            left: 0,
+            // backgroundImage: `url(${IMAGE})`,
+            filter: 'blur(15px)',
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: 'blur(20px)',
+            },
+          }}>
+          <Image
+            rounded={'lg'}
+            height="auto"
+            width="100%"
+            objectFit={'cover'}
+            src={image}
+          />
+        </Box>
+        <Stack pt={10} align={'center'}>
+          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={300}>
+            {title}
+          </Heading>
+          <Text color={'gray.500'} fontSize={'lg'} textTransform={'uppercase'}>
+            {category}
+          </Text>
+          <Stack direction={'row'} align={'center'}>
+            <Text fontWeight={800} fontSize={'xl'}>
+              $ {price}
+            </Text>
+          </Stack>
+        </Stack>
+      </Box>
+    </Center>
+  );
+}
 
 export default function Products(){
-    const [data,setData] = useState([])
+    const [data,setData] = useState([]);
+    const [page,setPage] = useState(1);
 
     useEffect(() => {
-        fetch(`http://localhost:8080/furniture`)
+        fetch(`http://localhost:8080/AllProducts`)
         .then((res) => res.json())
         .then((res) => setData(res))
         .catch((err) => console.log(err)) 
@@ -12,15 +89,26 @@ export default function Products(){
 
 
 
-    return <div style={{
-        display:"grid",
-        gridTemplateColumns:"repeat(3,1fr)",
-        gap:"25px"
-    }}>
-        {data?.map((e) => {
-            return <div key={e.id}>
-                <img src={e.image} alt={e.title} />
+    return <div style={{display:"flex"}}>
+            <div>
+                <Sidebar/>
             </div>
-        })}
+            <div style={{
+                display:"grid",
+                gridTemplateColumns:"repeat(2,1fr)",
+                gap:"25px"
+            }}>
+            {data?.map((e) => {
+                return <div key={e.id} style={{border:"1px solid black"}} >
+                    {/* <ProductCard id={e.id} image={e.image} title={e.title} category={e.category} price={e.price}/> */}
+                    <SingleProduct id={e.id} image={e.image} title={e.title} category={e.category} price={e.price}/>
+                </div>
+            })}
+        </div>
     </div>
 }
+
+{/* <Image src={e.image} alt={e.title} width="85%"/>
+                    <Text fontSize="lg" fontWeight="bold">{e.title}</Text>
+                    <Text fontSize="l" fontWeight="semibold">{e.category}</Text>
+                    <Text fontSize="xl" fontWeight="bold">{e.price}</Text> */}
